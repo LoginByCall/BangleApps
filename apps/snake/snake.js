7a497cf1235e13b02804600f1a3c8a433a9bd29a
Alexander <LoginByCall@users.noreply.github.com>
2021-11-21 20:38:49 +0300

    Update snake.js
    Original control method change to alternative control method.

--- apps/snake/snake.js
+++ apps/snake/snake.js
@@ -4,7 +4,7 @@ const H = g.getWidth();
 const W = g.getHeight();
 let running = true;
 let score = 0;
-let d;
+let d = "r";
 const gridSize = 20;
 const tileSize = 6;
 let nextX = 0;
@@ -111,14 +111,76 @@ function draw() {
   g.flip();
 }
 
-// input
+// Alternative control method.
+setWatch(() => {
+  switch (d){
+    case "u": {
+      nextX = -1;
+      nextY = 0;
+      d = "l";
+      }
+      break;
+    case "r": {
+      nextX = 0;
+      nextY = 1;
+      d = "u";
+      }
+      break;
+    case "d": {
+      nextX = 1;
+      nextY = 0;
+      d = "r";
+      }
+      break;
+    case "l": {
+      nextX = 0;
+      nextY = -1;
+      d = "d";
+      }
+      break;
+  }
+}, BTN3, { repeat: true });
+
+setWatch(() => {
+  switch (d){
+    case "u": {
+      nextX = 1;
+      nextY = 0;
+      d = "r";
+      }
+      break;
+    case "r": {
+      nextX = 0;
+      nextY = -1;
+      d = "d";
+      }
+      break;
+    case "d": {
+      nextX = -1;
+      nextY = 0;
+      d = "l";
+      }
+      break;
+    case "l": {
+      nextX = 0;
+      nextY = 1;
+      d = "u";
+      }
+      break;
+  }
+}, BTN1, { repeat: true });
+
+
+// Original control method. 
+/*
 setWatch(() => {// Up
   if (d !== 'd') {
     nextX = 0;
     nextY = -1;
     d = 'u';
   }
-}, BTN1, { repeat: true });
+}, BTN, { repeat: true });
+
 setWatch(() => {// Down
   if (d !== 'u') {
     nextX = 0;
@@ -126,6 +188,7 @@ setWatch(() => {// Down
     d = 'd';
   }
 }, BTN3, { repeat: true });
+
 setWatch(() => {// Left
   if (d !== 'r') {
     nextX = -1;
@@ -133,6 +196,7 @@ setWatch(() => {// Left
     d = 'l';
   }
 }, BTN4, { repeat: true });
+
 setWatch(() => {// Right
   if (d !== 'l') {
     nextX = 1;
@@ -140,6 +204,8 @@ setWatch(() => {// Right
     d = 'r';
   }
 }, BTN5, { repeat: true });
+*/
+
 setWatch(() => {// Pause
   running = !running;
 }, BTN2, { repeat: true });
